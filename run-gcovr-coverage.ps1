5b80fa4d0109721c34ae0e61dfcfe2011a5e2f24
Harold Groenenboom <harold.groenenboom@splice.com>
2021-08-31 11:01:45 +0200

    Improved gcovr coverage script

--- run-gcovr-coverage.ps1
+++ run-gcovr-coverage.ps1
@@ -2,9 +2,20 @@
 # pip install gcovr
 # Add 'C:\Users\harol.DESKTOP-P3APK3S\AppData\Roaming\Python\Python37\Scripts' to path
 
+Remove-Item -Recurse *.exe
+
 g++ main.cpp --coverage -g -O0
 
 ./a.exe
 
 gcovr -r . --output coverage.txt
 gcovr -r . --json-summary --output coverage.json
+
+$json = Get-Content coverage.json | ConvertFrom-Json
+ConvertTo-Json -depth 100 $json | Out-File coverage.json
+
+Write-Host $json.line_percent
+if($json.line_percent -lt 50)
+{
+	Write-Error "Coverage of $($json.line_percent)% is not enough!"
+}
