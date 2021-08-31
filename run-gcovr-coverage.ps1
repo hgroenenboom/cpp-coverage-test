e19b1d96257e15a4941458d6d9507460265c2e48
Harold Groenenboom <harold.groenenboom@splice.com>
2021-08-31 10:49:09 +0200

    improved gcovr script

--- run-gcovr-coverage.ps1
+++ run-gcovr-coverage.ps1
@@ -1,8 +1,10 @@
 # https://gcovr.com/en/stable/guide.html#the-gcovr-command
+# pip install gcovr
+# Add 'C:\Users\harol.DESKTOP-P3APK3S\AppData\Roaming\Python\Python37\Scripts' to path
 
 g++ main.cpp --coverage -g -O0
 
 ./a.exe
 
-gcovr --output coverage.txt
-gcovr --json-summary --output coverage.json
+gcovr -r . --output coverage.txt
+gcovr -r . --json-summary --output coverage.json
