ec8bc5b2c122b3be5c871e6a1a73b6c9bd0aaa31
Harold Groenenboom <harold.groenenboom@splice.com>
2021-09-01 10:44:34 +0200

    another attempt

--- run-coveralls-coverage.ps1
+++ run-coveralls-coverage.ps1
@@ -6,14 +6,25 @@ param(
 # pip install gcovr
 # Add 'C:\Users\harol.DESKTOP-P3APK3S\AppData\Roaming\Python\Python37\Scripts' to path
 
-cd $PSScriptRoot
+Set-Location $PSScriptRoot
 # git clean -dXf
 
+Write-Host "--- Cloning and installing cpp-coveralls"
 git clone https://github.com/eddyxu/cpp-coveralls
 pip install --user cpp-coveralls
 
+Write-Host "--- Building program"
 g++ main.cpp helper.cpp --coverage -g -O0
 
-./a.exe
+Write-Host "--- Executing program to obtain coverage information"
+if($IsLinux)
+{
+    ./a.out
+}
+else 
+{
+    ./a.exe
+}
 
+Write-Host "--- Running cpp-coveralls to upload coverage report"
 coveralls -r . --verbose --repo-token $COVERALLS_REPO_TOKEN
